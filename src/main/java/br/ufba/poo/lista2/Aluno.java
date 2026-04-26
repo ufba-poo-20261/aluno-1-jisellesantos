03874f2526e240b6596260969003a37c323930ef
jisellesantos <jisellesantos@ufba.br>
2026-04-26 11:49:19 -0300

    Jiselle Aluno.java

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -16,7 +16,33 @@ package br.ufba.poo.lista2;
  */
 public class Aluno {
 
+    private final String matricula;
+    private String nome;
+
     public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Aluno aluno = (Aluno) o;
+        return matricula.equals(aluno.matricula);
     }
 }
 
